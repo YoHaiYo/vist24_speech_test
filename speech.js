40756fde5d166063a2a72e04c02252db7ee90833
YoHaiYo <12si47bun@naver.com>
2024-09-05 08:45:53 +0900

    음성인식안되서 리턴

--- speech.js
+++ speech.js
@@ -5,9 +5,7 @@ recognition.lang = "ko-KR"; // 음성 인식 언어 설정
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-// 음성 인식 활성 상태를 추적하는 변수
-let isRecognitionActive = false; // 음성 인식이 진행 중인지 여부를 저장
-
+// chatbot.js에 이미 정의 되어있음. 나중에 처리하기
 const voiceButton = document.querySelector(".voice-button");
 const inputBoxSelect = document.querySelector(".input-box-select");
 
@@ -15,15 +13,7 @@ const inputBoxSelect = document.querySelector(".input-box-select");
 // 음성 인식을 시작하는 함수
 function startSpeechRecognition() {
   recognition.start();
-  isRecognitionActive = true; // 음성 인식이 시작됨을 표시
   console.log("음성 입력을 기다리는 중...");
-}
-
-// 음성 인식을 종료하는 함수
-function stopSpeechRecognition() {
-  recognition.stop();
-  isRecognitionActive = false; // 음성 인식이 종료됨을 표시
-  console.log("음성 입력을 중단함...");
 }
 
 // 음성 음식 버튼 활성화 토글
@@ -46,19 +36,12 @@ function handleSpeechError(event) {
 // 음성 인식이 끝났을 때의 함수
 function handleSpeechEnd() {
   console.log("음성 인식 종료");
-  isRecognitionActive = false; // 음성 인식 종료 상태로 업데이트
 }
 
-/* 3. DOMContentLoaded 이벤트 핸들링 */
+/* 3. DOMContentLoaded 이벤트 핸들링 (간결하게) */
 document.addEventListener("DOMContentLoaded", function () {
-  // 버튼 클릭 시 음성 인식 시작 또는 종료
-  voiceButton.addEventListener("click", function () {
-    if (isRecognitionActive) {
-      stopSpeechRecognition(); // 음성 인식이 활성 상태라면 종료
-    } else {
-      startSpeechRecognition(); // 비활성 상태라면 시작
-    }
-  });
+  // 버튼 클릭 시 음성 인식 시작
+  voiceButton.addEventListener("click", startSpeechRecognition);
   voiceButton.addEventListener("click", voiceButtonActiveToggle);
 
   // 음성 인식 결과 및 오류 핸들링 설정
